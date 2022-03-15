e87c205eea8cadbcfdd41ad21d3a511505b34e41
Vincent Roth <vincent@placeme.io>
2022-03-15 12:43:20 +0100

    Intercepteur HTTP

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MailToDirective } from './mail-to.directive';
 import { PhonePipe } from './phone.pipe';
+import { TimeInterceptor } from './api/time.interceptor';
 
 @NgModule({
   declarations: [MailToDirective, PhonePipe],
   imports: [CommonModule, HttpClientModule],
   exports: [MailToDirective, PhonePipe],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
+  ],
 })
 export class SharedModule {}
