647378156cbb071f516e957d0afefa00d1c6b303
Vincent Roth <vincent@placeme.io>
2022-03-15 15:53:41 +0100

    Routes enfants

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -5,12 +5,14 @@ import { AnimalDetailsComponent } from './animal-details/animal-details.componen
 import { AnimalListItemComponent } from './animal-list-item/animal-list-item.component';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { AnimalRoutingModule } from './animal-routing.module';
+import { AnimalComponent } from './animal.component';
 
 @NgModule({
   declarations: [
     AnimalDetailsComponent,
     AnimalListComponent,
     AnimalListItemComponent,
+    AnimalComponent,
   ],
   imports: [CommonModule, SharedModule, AnimalRoutingModule],
 })
